e75d93cfcc4c531af469001bebca92e6f483e754
Nupur Rana <nupurrana123@gmail.com>
2026-03-31 10:46:25 +0530

    char frequency

---
+++ python_assessment_py/count_frequency_using_dictionary.py
@@ -0,0 +1,12 @@
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+# Example usage
+text = "programming"
+print("Character frequency:", char_frequency(text))
